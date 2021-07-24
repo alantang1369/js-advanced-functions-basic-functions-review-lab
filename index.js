cae6097406f590f8ed9804537ad2b7a9c757d040
Alan Tang <giabao_tang@yahoo.com>
2021-07-23 17:48:35 -0700

    commit tree 70c9ffe7074590c307915bf885e7158b778ff94f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function saturdayFun(thing="roller-skate"){
+     return `This Saturday, I want to ${thing}!`;
+}
+const mondayWork = function(thing="go to the office"){
+    return `This Monday, I will ${thing}.`;
+}
+function wrapAdjective(visual="*"){
+    return function(para="special"){
+        return `You are ${visual}${para}${visual}!`
+    }
+}
+
+let Calculator = {
+    add: function(a,b){return a + b},
+    subtract: function(a,b){return a-b},
+    multiply: function(a,b){return a*b},
+    divide: function(a,b){ return a/b}
+}
+function actionApplyer(num, array){
+    let result = num;
+    for (let i=0; i < array.length;i++) {
+        result = array[i](result)
+    }
+    return result;
+
+}
